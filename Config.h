d093e9dc35335dd57a0c7a7a0266d3c2368c6d3f
Jonathan Naylor <naylorjs@yahoo.com>
2023-09-01 15:39:21 +0100

    Use the IL2P CRC on transmit always.

--- Config.h
+++ Config.h
@@ -67,9 +67,6 @@
 // Set the mode 2 transmit level (out of 255)
 #define	MODE2_TX_LEVEL	128
 
-// Use the new CRC option on IL2P transmit, receive automatically detects its use
-#define	USE_IL2P_CRC		1
-
 // Use pins to output the current mode via LEDs
 #define MODE_LEDS
 
