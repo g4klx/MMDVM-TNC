dabd86dc961662ac1442e0a359dddda969517611
Jonathan Naylor <naylorjs@yahoo.com>
2023-10-14 15:33:18 +0100

    Increase the TX and RX ringbuffer sizes.

--- Globals.h
+++ Globals.h
@@ -53,8 +53,8 @@
 
 const uint16_t RX_BLOCK_SIZE = 2U;
 
-const uint16_t TX_RINGBUFFER_SIZE = 500U;
-const uint16_t RX_RINGBUFFER_SIZE = 600U;
+const uint16_t TX_RINGBUFFER_SIZE = 1000U;
+const uint16_t RX_RINGBUFFER_SIZE = 2432U;
 
 #if defined(STM32F105xC) || defined(__MK20DX256__)
 const uint16_t TX_BUFFER_LEN = 2000U;
